3b19b83688541676bde09ab8df94fbb08136eb34
Matt Mazzola <mattmazzola@live.com>
2016-05-20 23:21:55 -0700

    Update application template to show logged in status.

---
+++ app/pods/application/controller.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+
+  actions: {
+    invalidateSession() {
+      this.get('session').invalidate();
+    },
+    login() {
+      console.log('login');
+    }
+  }
+});
