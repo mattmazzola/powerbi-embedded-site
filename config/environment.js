b76d74103285e75352e12eb9f0215bd719eaf7fd
Matt Mazzola <mattmazzola@live.com>
2016-05-20 22:55:04 -0700

    Configure app to use pods.

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'powerbi-embedded-site',
+    podModulePrefix: 'powerbi-embedded-site/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
