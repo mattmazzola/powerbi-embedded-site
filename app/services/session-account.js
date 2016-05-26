01425d500220f5e3b3890667a6800bd6d1b25386
Matt Mazzola <mattmazzola@live.com>
2016-05-25 23:36:14 -0700

    Update session-account to expose photo url and graph resources

--- app/services/session-account.js
+++ app/services/session-account.js
@@ -1,9 +1,27 @@
 import Ember from 'ember';
 
+const {
+  computed
+} = Ember;
+
 export default Ember.Service.extend({
   session: Ember.inject.service('session'),
   
-  hasPowerBiLicense: Ember.computed('session.data.authenticated.user.assignedPlans', function() {
+  photoUrl: computed('microsoftGraph', function () {
+    return `${this.get('microsoftGraph.endpointUri')}v1.0/me/photo/$value`;
+  }),
+  
+  microsoftGraph: computed('session.data.authenticated.resources', function () {
+    const resources = this.get('session.data.authenticated.resources');
+    return resources.find(resource => resource.endpointUri === "https://graph.microsoft.com/");
+  }),
+  
+  azureGraph: computed('session.data.authenticated.resources', function () {
+    const resources = this.get('session.data.authenticated.resources');
+    return resources.find(resource => resource.endpointUri === "https://graph.windows.net/");
+  }),
+  
+  hasPowerBiLicense: computed('session.data.authenticated.user.assignedPlans', function() {
     const assignedPlans = this.get('session.data.authenticated.user.assignedPlans');
     
     const powerBiPlan = assignedPlans.find(plan => plan.service === "PowerBI");
