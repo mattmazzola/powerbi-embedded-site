6ac465cb12769d1d3dedb8650e2fa8d438685b58
Matt Mazzola <mattmazzola@live.com>
2016-05-25 23:36:43 -0700

    Update routes with simple-auth mixins

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ApplicationRouteMixin, {
   session: Ember.inject.service('session'),
   sessionAccount: Ember.inject.service('session-account'),
 
