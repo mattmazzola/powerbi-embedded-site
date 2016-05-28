590cdccf1b163ba981c499da5ece86c03de957ab
Matt Mazzola <mattmazzola@live.com>
2016-05-28 15:09:00 -0700

    Update index page to allow fetching subscriptions and resourceGroups

--- app/pods/index/controller.js
+++ app/pods/index/controller.js
@@ -1,5 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service('session')
+  store: Ember.inject.service('store'),
+  session: Ember.inject.service('session'),
+  subscriptions: null,
+  resourceGroups: null,
+  selectedSubscription: null,
+  selectedResourceGroup: null,
+  
+  actions: {
+    loadSubscriptions() {
+      this.set('subscriptions', this.store.findAll('subscription'));
+    },
+    
+    loadResourceGroups() {
+      const resourceGroups = this.get('selectedSubscription').get('resourceGroups');
+      
+      console.log(resourceGroups);
+      
+      // this.set('resourceGroups', this.store.query('resourceGroup', { subscription: this.get('selectedSubscription') }));
+    },
+    
+    selectSubscription(subscription) {
+      this.set('selectedSubscription', subscription);
+    },
+    
+    selectResourceGroup(resourceGroup) {
+      this.set('selectedResourceGroup', resourceGroup);
+    }
+  }
 });
