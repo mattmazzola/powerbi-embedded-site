daed059eb2fd20ed72eddd8b24a1dcae44af76de
Matt Mazzola <mattmazzola@live.com>
2016-06-05 20:22:07 -0700

    Remove unecessary override of buildUrl in resource group adapter since it inherits from subscription

--- app/adapters/resource-group.js
+++ app/adapters/resource-group.js
@@ -1,49 +1,5 @@
 import SubscriptionAdapter from './subscription';
 
 export default SubscriptionAdapter.extend({
-  namespace: 'subscriptions',
-  
-  buildURL(modelName, id, snapshot, requestType, query) {
-    let url;
-    
-    switch (requestType) {
-      case 'findRecord':
-        url = this.urlForFindRecord(id, modelName, snapshot);
-        break;
-      case 'findAll':
-        url = this.urlForFindAll(modelName, snapshot);
-        break;
-      case 'query':
-        url = this.urlForQuery(query, modelName);
-        break;
-      case 'queryRecord':
-        url = this.urlForQueryRecord(query, modelName);
-        break;
-      case 'findMany':
-        url = this.urlForFindMany(id, modelName, snapshot);
-        break;
-      case 'findHasMany':
-        url = this.urlForFindHasMany(id, modelName, snapshot);
-        break;
-      case 'findBelongsTo':
-        url = this.urlForFindBelongsTo(id, modelName, snapshot);
-        break;
-      case 'createRecord':
-        url = this.urlForCreateRecord(modelName, snapshot);
-        break;
-      case 'updateRecord':
-        url = this.urlForUpdateRecord(id, modelName, snapshot);
-        break;
-      case 'deleteRecord':
-        url = this.urlForDeleteRecord(id, modelName, snapshot);
-        break;
-      default:
-        url = this._buildURL(modelName, id);
-        break;
-    }
-    
-    url += `?api-version=2016-01-29`;
-    
-    return url;
-  }
+  namespace: 'subscriptions'
 });
