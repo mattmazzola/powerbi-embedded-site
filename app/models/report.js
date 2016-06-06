003e05a7e517f203bdb73ef618c698b53c0d71a5
Matt Mazzola <mattmazzola@live.com>
2016-06-06 00:00:29 -0700

    Add ability to get access / embed token for reports

--- app/models/report.js
+++ app/models/report.js
@@ -1,11 +1,72 @@
+import Ember from 'ember';
+import fetch from 'ember-network/fetch';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
+
+const {
+  computed
+} = Ember;
 
 export default Model.extend({
   embedUrl: attr('string'),
   name: attr('string'),
   webUrl: attr('string'),
   
-  workspace: belongsTo('workspace')
+  workspace: belongsTo('workspace'),
+  
+  tokens: null,
+  
+  init() {
+    this._super();
+    this.set('tokens', []);
+  },
+  
+  generateEmbedToken() {
+    const createEmbedTokenRequest = {
+      url: 'http://localhost:1249/api/generateembedtoken',
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        workspaceCollectionName: this.get('workspace.workspaceCollection.id'),
+        workspaceId: this.get('workspace.id'),
+        reportId: this.get('id'),
+        accessKey: this.get('workspace.workspaceCollection.accessKeys.key1')
+      })
+    };
+    
+    return fetch(createEmbedTokenRequest.url, createEmbedTokenRequest)
+      .then(response => {
+        const json = response.json();
+        if(response.ok) {
+          return json;
+        }
+        
+        throw json;
+      });
+  },
+  
+  accessToken: computed(function () {
+    let promise;
+    
+    if(this.get('tokens').length > 0) {
+      promise = Ember.Promise.resolve(this.get('tokens').get('lastObject'));
+    }
+    else {
+      promise = this.generateEmbedToken()
+        .then(token => {
+          this.get('tokens').pushObject(token);
+          return token;
+        });
+    }
+      
+    const ObjectPromiseProxy = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin);
+
+    return ObjectPromiseProxy.create({
+      promise
+    });
+  })
 });
