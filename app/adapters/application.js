909a06f721f331517c3557e3a2551366906d53a2
Matt Mazzola <mattmazzola@live.com>
2016-06-05 20:22:25 -0700

    Change default application adapter to use RESTAdapter to match serializers

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,4 @@
-import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import RESTAdapter from 'ember-data/adapters/rest';
 
-export default JSONAPIAdapter.extend({
+export default RESTAdapter.extend({
 });
