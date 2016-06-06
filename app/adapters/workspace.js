0a7e2cc785c78e217b181bd431ed2a9b2ff87f76
Matt Mazzola <mattmazzola@live.com>
2016-06-05 20:21:40 -0700

    Add adapter for workspace to override the findHasMany

---
+++ app/adapters/workspace.js
@@ -0,0 +1,32 @@
+import SubscriptionAdapter from './subscription';
+
+export default SubscriptionAdapter.extend({
+  /**
+   * Not sure why this is needed? but without it, headers are overwritten with base adapters headers
+   * findLink > findHasMany > this.ajax > this.ajaxOptions > this.headers() (The onBeforeSend functions set headers result on top of options)
+   */
+  headers() {
+    console.log('workspace#headers');
+    
+    return {
+      'Authorization': `AppToken xyz`
+    };
+  },
+  
+  findHasMany(store, snapshot, url, relationship) {
+    console.log(`workspace#findHasMany`);
+    var id   = snapshot.id;
+    var type = snapshot.modelName;
+
+    url = this.urlPrefix(url, this.buildURL(type, id, snapshot, 'findHasMany'));
+    
+    return snapshot.record.get('token')
+      .then(token => {
+        return this.ajax(url, 'GET', {
+          headers: {
+            'Authorization': `AppToken ${token}`
+          }
+        });
+      });
+  }
+});
