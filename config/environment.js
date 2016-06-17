4e3f3de3596ea13c054d0f7caad9e531248116af
Matt Mazzola <mattmazzola@live.com>
2016-06-17 01:04:53 -0700

    Add development specific redirectUrl

--- config/environment.js
+++ config/environment.js
@@ -40,6 +40,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.torii.providers.aad.redirectUri = 'https://powerbiembeddedsite.azurewebsites.net/'
   }
 
   if (environment === 'test') {
