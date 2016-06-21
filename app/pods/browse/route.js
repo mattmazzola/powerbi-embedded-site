72c16a2f84767202077b7e4ca305984c51c09788
Matt Mazzola <mattmazzola@live.com>
2016-06-21 02:20:54 -0700

    Remove load subscriptions button and make load part of route

--- app/pods/browse/route.js
+++ app/pods/browse/route.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model() {
+    return this.store.findAll('subscription');
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('subscriptions', model);
+  }
 });
